b9ff4ee6e0e47e9d1d1124f76674a85d7597fbbc
Sharad <sharadh1991@gmail.com>
2021-03-07 18:12:05 +0530

    WI

---
+++ src/testPckage/Addition.java
@@ -0,0 +1,13 @@
+package testPckage;
+
+public class Addition 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is first program");
+		int a=10,b=9,c;
+		c=a+b;
+		System.out.println("Addition of numbers is =="+c);		
+	}
+
+}
---
+++ src/testPckage/Hello.java
@@ -0,0 +1,11 @@
+package testPckage;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
